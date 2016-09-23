40fc0d0175f1d29d7dfd4734eb81edef91ce0724
yj <DS@603-06>
2016-09-23 16:15:47 +0900

    메인

--- Preorder/src/main/java/controller/ControllerLogin.java
+++ Preorder/src/main/java/controller/ControllerLogin.java
@@ -1,5 +1,18 @@
 package controller;
 
-public class ControllerLogin {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class ControllerLogin {
+	
+
+	@RequestMapping("/")
+	public String home() {
+		
+
+		
+		return "main";
+	}
+	
 }
